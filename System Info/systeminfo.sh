bc8dba86df6a1461877445afd54aeabadd043d09
Jeffrin Goldwin <jeffcrjj@gmail.com>
2026-09-15 17:40:12 +0000

    dev: added Memory Usage, Disk Usage, Uptime and IP Address

--- System Info/systeminfo.sh
+++ System Info/systeminfo.sh
@@ -7,9 +7,15 @@ os=$(uname)
 kernal=$(uname -s)
 cpu=$(top -bn1 | awk -F "id," '/Cpu/ {print $1}' | awk -F "," '{print 100-$NF}')
 mem=$(free -m | awk ' /Mem:/ {printf "%.2f\n", $3/$2*100}')
+disk=$(df -h | awk ' /C:\\/  { print $5}')
+uptime=$(uptime -p | awk '{sub(/^up /, ""); print $0}')
+ip=$(hostname -I)
 
 echo "host $hostname"
 echo "os $os"
 echo "kernal $kernal"
-echo "Cpu% $cpu"
-echo "Mem% $mem"
+echo "Cpu Usage $cpu"
+echo "Memory Usage $mem"
+echo "Disk Usage $disk"
+echo "Uptime $uptime"
+echo "IP Adress $ip"
