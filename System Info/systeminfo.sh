f24e603984f04466a49e507181d8106050825e51
Jeffrin Goldwin <jeffcrjj@gmail.com>
2026-09-15 17:20:51 +0000

    dev: added awk command for memeory utilization

--- System Info/systeminfo.sh
+++ System Info/systeminfo.sh
@@ -6,8 +6,10 @@ hostname=$(hostname)
 os=$(uname)
 kernal=$(uname -s)
 cpu=$(top -bn1 | awk -F "id," '/Cpu/ {print $1}' | awk -F "," '{print 100-$NF}')
+mem=$(free -m | awk ' /Mem:/ {printf "%.2f\n", $3/$2*100}')
 
 echo "host $hostname"
 echo "os $os"
 echo "kernal $kernal"
-echo "CPU% $cpu"
+echo "Cpu% $cpu"
+echo "Mem% $mem"
